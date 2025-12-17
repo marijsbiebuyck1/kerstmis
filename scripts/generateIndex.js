d36f07d7647bb2469cb2a05c1f2220147de475db
Marijs Biebuyck <marijs.biebuyck@telenet.be>
2025-12-17 18:11:10 +0100

    font

--- scripts/generateIndex.js
+++ scripts/generateIndex.js
@@ -7,6 +7,10 @@ const content = `<!doctype html>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Kerstcadeau</title>
+    <!-- Preconnect and load Google Fonts for Reenie Beanie (improves mobile reliability) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Reenie+Beanie&display=swap" rel="stylesheet">
   </head>
   <body>
     <div id="root"></div>
